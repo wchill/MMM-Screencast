148b68938fc014de0fca3f9c68662a97cbc53cda
Eric Ahn <erah@microsoft.com>
2022-06-30 15:21:03 -0700

    logging statements

--- constants.js
+++ constants.js
@@ -7,28 +7,6 @@ const MODULE_NOTIFICATIONS = {
     config_error: 'MMM-Screencast:CONFIG-ERROR'
 };
 
-const POSITIONS = {
-    bottomRight: 'bottomRight',
-    bottomCenter: 'bottomCenter',
-    bottomLeft: 'bottomLeft',
-    center: 'center',
-    topRight: 'topRight',
-    topCenter: 'topCenter',
-    topLeft: 'topLeft',
-    top_bar: 'topRight',
-    top_left: 'topLeft',
-    top_center: 'topCenter',
-    top_right: 'topRight',
-    upper_third: 'topRight',
-    middle_center: 'center',
-    lower_third: 'bottomCenter',
-    bottom_left: 'bottomLeft',
-    bottom_center: 'bottomCenter',
-    bottom_right: 'bottomRight',
-    bottom_bar: 'bottomCenter'
-};
-
 module.exports = {
-    MODULE_NOTIFICATIONS,
-    POSITIONS
+    MODULE_NOTIFICATIONS
 };
